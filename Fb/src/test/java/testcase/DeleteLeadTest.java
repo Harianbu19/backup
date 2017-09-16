2f3d8345bc8ea99116ad9f9ebb0e94ca56d43b80
Harianbu19 <31177620+Harianbu19@users.noreply.github.com>
2017-09-16 18:55:20 +0530

    Add files via upload

---
+++ Fb/src/test/java/testcase/DeleteLeadTest.java
@@ -0,0 +1,11 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+public class DeleteLeadTest {
+
+  @Test
+  public void Login() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
